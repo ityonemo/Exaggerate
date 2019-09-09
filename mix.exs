3b3fe137a0f19d377f46358fdc155473823f8bc1
Isaac Yonemoto <isaac.yonemoto@gmail.com>
2019-09-09 13:28:07 -0700

    point it to a different exonerate and repoint lock file

--- mix.exs
+++ mix.exs
@@ -21,7 +21,7 @@ defmodule Exaggerate.Mixfile do
     [
       {:dialyxir, "~> 1.0.0-rc.4", only: :dev, runtime: false},
       {:plug_cowboy, "~> 1.0"},
-      {:exonerate, git: "https://github.com/rstorlabs/exonerate.git", branch: "use_macros"},
+      {:exonerate, git: "https://github.com/ityonemo/exonerate.git"},
       {:httpoison, "~> 1.4", only: :test},
       {:jason, "~> 1.1"},
       {:plug, "~> 1.7"},
